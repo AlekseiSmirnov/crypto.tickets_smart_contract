1a5f6349552a01ace148787a207f997b7a44a9d7
Anthony Akentiev <anthony.akentiev@gmail.com>
2017-09-03 16:53:38 +0300

    Fixing approve

--- contracts/ICO.sol
+++ contracts/ICO.sol
@@ -285,6 +285,12 @@ contract TKT  is ERC20 {
 
 
     function approve(address _spender, uint256 _amount) returns (bool) {
+        // To change the approve amount you first have to reduce the addresses`
+        //  allowance to zero by calling `approve(_spender, 0)` if it is not
+        //  already 0 to mitigate the race condition described here:
+        //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+        require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+
         allowed[msg.sender][_spender] = _amount;
         Approval(msg.sender, _spender, _amount);
         return true;

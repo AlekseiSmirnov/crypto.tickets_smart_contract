c501a3c2842d060fd04f411e9405c1b50f25918c
Anthony Akentiev <anthony.akentiev@gmail.com>
2017-09-03 16:51:17 +0300

    Remove explicit ret.values

--- contracts/ICO.sol
+++ contracts/ICO.sol
@@ -51,11 +51,11 @@ contract ERC20 {
     mapping(address => uint256) balances;
     mapping(address => mapping (address => uint256)) allowed;
 
-    function balanceOf(address _owner) constant returns (uint balance);
-    function transfer(address _to, uint _value) returns (bool success);
-    function transferFrom(address _from, address _to, uint _value) returns (bool success);
-    function approve(address _spender, uint _value) returns (bool success);
-    function allowance(address _owner, address _spender) constant returns (uint remaining);
+    function balanceOf(address _owner) constant returns (uint);
+    function transfer(address _to, uint _value) returns (bool);
+    function transferFrom(address _from, address _to, uint _value) returns (bool);
+    function approve(address _spender, uint _value) returns (bool);
+    function allowance(address _owner, address _spender) constant returns (uint);
 
     event Transfer(address indexed _from, address indexed _to, uint _value);
     event Approval(address indexed _owner, address indexed _spender, uint _value);
@@ -246,12 +246,12 @@ contract TKT  is ERC20 {
     }
 
 
-    function balanceOf(address _owner) constant returns (uint256 balance) {
+    function balanceOf(address _owner) constant returns (uint256) {
          return balances[_owner];
     }
 
 
-    function transfer(address _to, uint256 _amount) returns (bool success) {
+    function transfer(address _to, uint256 _amount) returns (bool) {
         require(!tokensAreFrozen);
         if (balances[msg.sender] >= _amount && _amount > 0 && balances[_to] + _amount > balances[_to])
         {
@@ -267,7 +267,7 @@ contract TKT  is ERC20 {
     }
 
 
-    function transferFrom(address _from, address _to, uint256 _amount) returns (bool success) {
+    function transferFrom(address _from, address _to, uint256 _amount) returns (bool) {
         require(!tokensAreFrozen);
         if (balances[_from] >= _amount && allowed[_from][msg.sender] >= _amount && _amount > 0 && balances[_to] + _amount > balances[_to])
         {
@@ -284,14 +284,14 @@ contract TKT  is ERC20 {
      }
 
 
-    function approve(address _spender, uint256 _amount) returns (bool success) {
+    function approve(address _spender, uint256 _amount) returns (bool) {
         allowed[msg.sender][_spender] = _amount;
         Approval(msg.sender, _spender, _amount);
         return true;
     }
 
 
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+    function allowance(address _owner, address _spender) constant returns (uint256) {
         return allowed[_owner][_spender];
     }
 }
